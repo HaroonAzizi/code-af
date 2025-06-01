beedfdb7ac35bcc2911b6ce5a1932b41d053dfb0
HaroonAzizi <haroonazizi15@gmail.com>
2025-06-01 22:20:44 +0430

    updates to price range

--- app/components/ContactCard.js
+++ app/components/ContactCard.js
@@ -156,10 +156,10 @@ export default function ContactCard() {
                     className="w-full px-4 py-3 bg-neutral-800/50 border border-neutral-700 rounded-lg text-white focus:border-blue-400 focus:outline-none transition-colors"
                   >
                     <option value="">Select budget range</option>
-                    <option value="5k-10k">$5,000 - $10,000</option>
-                    <option value="10k-25k">$10,000 - $25,000</option>
-                    <option value="25k-50k">$25,000 - $50,000</option>
-                    <option value="50k+">$50,000+</option>
+                    <option value="50-499">$50 - $499</option>
+                    <option value="500-1999">$500 - $1999</option>
+                    <option value="2000-4999">$2000 - $4999</option>
+                    <option value="5000+">$5000+</option>
                   </select>
                 </div>
               </div>
